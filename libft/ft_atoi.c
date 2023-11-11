8c4fe6532d600e3e48d6ea65b1ad7f578ccf00a0
jeromeDev94 <jeromep.dev@gmail.com>
2023-11-11 13:40:17 +0100

    ft_atoi v1 done, need to make test before implementation confirmation

---
+++ libft/ft_atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: JeromeP <JeromeP@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 03:30:38 by JeromeP           #+#    #+#             */
+/*   Updated: 2023/11/11 13:30:45 by JeromeP          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(int c)
+{
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
+
+static int	ft_issign(int c, int *sign)
+{
+	if (c == '+')
+		return (1);
+	if (c == '-')
+	{
+		*sign *= -1;
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	result;
+	int	digit;
+	int	sign;
+	int	i;
+
+	i = 0;
+	digit = 0;
+	result = 0;
+	sign = 1;
+	if (ft_isspace(nptr[i]))
+		i++;
+	if (ft_issign(nptr[i], &sign))
+		i++;
+	while(nptr[i])
+	{
+		digit = nptr[i] - '0';
+		result = result * 10 + digit;
+		i++;
+	}
+	return (result * sign);
+}
