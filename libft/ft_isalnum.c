4dd2ac1db7348143af24903818a658e751026651
jeromeDev94 <jeromep.dev@gmail.com>
2023-11-11 18:09:34 +0100

    maj libft to fit the good 42header + fonction ok, ready for test + ft_atoi test ok

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: JeromeP <JeromeP@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/11 14:33:43 by JeromeP           #+#    #+#             */
-/*   Updated: 2023/11/11 14:57:18 by JeromeP          ###   ########.fr       */
+/*   Created: 2023/11/11 18:03:56 by jeportie          #+#    #+#             */
+/*   Updated: 2023/11/11 18:04:03 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	ft_isalnum(int c)
 {
 	if (ft_isalpha(c) || ft_isdigit(c))
-		return(1);
+		return (1);
 	return (0);
 }
