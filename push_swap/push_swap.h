3f10f716b1f5217363a1bf02d04803507b8aab71
Jerome Portier <jeportie@paul-f4Br3s2.clusters.42paris.fr>
2024-03-19 16:32:27 +0100

    start of push_swap project.

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jeportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:24:54 by jeportie          #+#    #+#             */
-/*   Updated: 2024/03/19 13:47:30 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/03/19 16:21:56 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,18 @@ typedef enum
 	true
 }	Bool;
 
-typedef struct	s_dc_node
+typedef struct	s_dcnode
 {
 	int	value;
-	struct s_dc_node	*back;
-	struct s_dc_node	*next;_
-}	t_dc_node;
+	struct s_dcnode	*back;
+	struct s_dcnode	*next;_
+}	t_dcnode;
 
 typedef struct s_dclst
 {
 	int	lenght;
-	struct t_dc_node	*begin;
-	struct t_dc_node	*end;
+	struct t_dcnode	*begin;
+	struct t_dcnode	*end;
 }	t_dclst;
 
 #endif /*PUSH_SWAP*/
