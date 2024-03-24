6445bc917b10e11f17f10e5744b7c640de0becf6
jeromeDev94 <jeromep.dev@gmail.com>
2024-03-24 14:14:13 +0100

    add ALL push_swap commamds and then work on the algo.

--- push_swap/src/radix_sort.c
+++ push_swap/src/radix_sort.c
@@ -6,88 +6,46 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:00:46 by jeportie          #+#    #+#             */
-/*   Updated: 2024/03/23 21:14:37 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/03/24 14:13:51 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 #include "../include/ft_printf.h"
 
+long	ft_labs(long value)
+{
+	if (value < 0)
+		return (-value);
+	return value;
+}
+
+int		ft_find_max_bits(t_dclst *stack)
+{
+	int		max_bits;
+	long	max_value;
+	t_dcnode *current;
+
+	max_bits = 0;
+	max_value = 0;
+	current = stack->begin;
+	while (current)
+	{
+		if (ft_labs(current->value) > max_value)
+			max_value = ft_labs(current->value);
+		current = current->next;
+	}
+	while (max_value >> max_bits)
+		max_bits++;
+	return (max_bits);
+}
+
+void	ft_apply_bitwise_sort(t_dclst *stack_a, t_dclst *stack_b, int bit)
+{
+
+}
+
 void	radix_sort(t_dclst *stack_a, t_dclst *stack_b)
 {
-    ft_printf("\n");
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_printf("\n");
-    ft_print_stack(stack_a, 'a');
-    ft_print_stack(stack_b, 'b');
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_printf("\n");
-    ss(stack_a, stack_b);
-    rr(stack_a, stack_b);
-    ft_printf("\n");
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_print_stack(stack_a, 'a');
-    ft_print_stack(stack_b, 'b');
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_printf("\n");
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_printf("\n");
-    pa(stack_a, stack_b);
-    pa(stack_a, stack_b);
-    pa(stack_a, stack_b);
-    ft_print_stack(stack_a, 'a');
-    ft_print_stack(stack_b, 'b');
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_printf("\n");
-    ss(stack_a, stack_b);
-    ft_printf("\n");
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_print_stack(stack_a, 'a');
-    ft_print_stack(stack_b, 'b');
-    pa(stack_a, stack_b);
-    pa(stack_a, stack_b);
-    pa(stack_a, stack_b);
-    ft_printf("\n");
-    rr(stack_a, stack_b);
-    ft_printf("\n");
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_print_stack(stack_a, 'a');
-    ft_print_stack(stack_b, 'b');
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_printf("\n");
-    rrr(stack_a, stack_b);
-    ft_printf("\n");
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_print_stack(stack_a, 'a');
-    ft_print_stack(stack_b, 'b');
-    ft_printf("\n");
-    pa(stack_a, stack_b);
-    pa(stack_a, stack_b);
-    pa(stack_a, stack_b);
-    ft_printf("\n");
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    pb(stack_a, stack_b);
-    ft_print_stack(stack_a, 'a');
-    ft_print_stack(stack_b, 'b');
+
 }
