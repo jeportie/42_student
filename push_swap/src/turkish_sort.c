bdb35a266a82263a95dd6cf21ea48be1ccf471bc
jeromeDev94 <jeromep.dev@gmail.com>
2024-04-01 21:15:07 +0200

    working on turkish sort algo - implementation done need to debug

--- push_swap/src/turkish_sort.c
+++ push_swap/src/turkish_sort.c
@@ -6,11 +6,12 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 16:44:12 by jeportie          #+#    #+#             */
-/*   Updated: 2024/04/01 16:35:33 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/04/01 21:07:16 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+#include "../include/ft_printf.h"
 
 void	turkish_sort(t_dclst *stack_a, t_dclst *stack_b)
 {
@@ -24,15 +25,15 @@ void	turkish_sort(t_dclst *stack_a, t_dclst *stack_b)
 	ft_push_initial_numbers(stack_a, stack_b);
 	while (stack_a->length > 3)
 	{
-		ft_init_push_cost(stack_a);
 		ft_define_target(stack_a, stack_b);
 		ft_isabove_median(stack_a, stack_b);
 		ft_calculate_push_cost(stack_a, stack_b);
 		ft_push_to_target(stack_a, stack_b);
+		ft_init_push_cost(stack_a);
 	}
 	ft_sort_three(stack_a);
-//	ft_back_to_stack_a(stack_a, stack_b);
-//	ft_fine_tune(stack_a);
+	ft_back_to_stack_a(stack_a, stack_b);
+	ft_fine_tune(stack_a);
 }
 
 void	ft_topless(t_dclst *stack_a, long top, long middle, long bottom)
@@ -178,7 +179,7 @@ void	ft_define_index(t_dclst *stack)
 	}
 }
 
-int	calculate_moves_to_top(t_dclst *stack, t_dcnode *node)
+int	ft_calculate_moves_to_top(t_dclst *stack, t_dcnode *node)
 {
 	int	moves;
 
@@ -196,8 +197,8 @@ void	ft_calculate_push_cost(t_dclst *stack_a, t_dclst *stack_b)
 	current_a = stack_a->begin;
 	while (current_a)
 	{
-		current_a->moves_a = calculate_moves_to_top(stack_a, current_a);
-		current_a->moves_b = calculate_moves_to_top(stack_b, current_a->target);
+		current_a->moves_a = ft_calculate_moves_to_top(stack_a, current_a);
+		current_a->moves_b = ft_calculate_moves_to_top(stack_b, current_a->target);
 		if (current_a->above_median == current_a->target->above_median)
 		{
 			if (current_a->moves_a < current_a->moves_b)
@@ -275,14 +276,95 @@ void	ft_init_push_cost(t_dclst *stack_a)
 		current_a = current_a->next;
 	}
 }
-/*
+
+t_dcnode*	ft_closest_greater_number(t_dcnode *node_b, t_dclst *stack_a)
+{
+	t_dcnode	*current_a;
+	t_dcnode	*target_a;
+	long		min_value;
+
+	current_a = stack_a->begin;
+	target_a = NULL;
+	min_value = LONG_MAX;
+	while (current_a)
+	{
+		if (current_a->value > node_b->value && current_a->value < min_value)
+		{
+			target_a = current_a;
+			min_value = current_a->value;
+		}
+		current_a = current_a->next;
+	}
+	if (!target_a)
+	{
+		current_a = stack_a->begin;
+		while (current_a)
+		{
+			if (current_a->value < min_value)
+			{
+				target_a = current_a;
+				min_value = current_a->value;
+			}
+			current_a = current_a->next;
+		}
+	}
+	return (target_a);
+}
+
 void	ft_back_to_stack_a(t_dclst *stack_a, t_dclst *stack_b)
 {
-	return ;
+	t_dcnode	*current_b;	
+	int			moves;
+	int			i;
+
+	current_b = stack_b->begin;
+	moves = 0;
+	while (current_b)
+	{
+		ft_actualise_indexes(stack_a, stack_b);
+		ft_isabove_median(stack_a, stack_b),
+		current_b->target = ft_closest_greater_number(current_b, stack_a);
+		moves = ft_calculate_moves_to_top(stack_a, current_b->target);
+		i = 0;
+		while (i < moves)
+		{
+			if (current_b->target->above_median)
+				ra(stack_a);
+			else
+				rra(stack_a);
+			i++;
+		}
+		pa(stack_a, stack_b);
+	}
 }
 
 void	ft_fine_tune(t_dclst *stack_a)
 {
-	return ;
+	t_dcnode	*current;
+	t_dcnode	*min_node;
+	long		min_value;
+	int			moves;
+
+	ft_define_index(stack_a);
+	ft_mark_above_median(stack_a);
+	current = stack_a->begin;
+	min_value = LONG_MAX;
+	min_node = NULL;
+	while (current)
+	{
+		if (current->value < min_value)
+		{
+			min_value = current->value;
+			min_node = current;
+		}
+		current = current->next;
+	}
+	moves = ft_calculate_moves_to_top(stack_a, min_node);
+	while (moves--)
+	{
+		if (min_node->above_median)
+			ra(stack_a);
+		else
+			rra(stack_a);
+	}
 }
-*/
