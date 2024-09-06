dbd8c504ccdd7175c44d19140bee259cd96783f5
Jerome Portier <jeportie@made-f0Br8s8.clusters.42paris.fr>
2024-09-06 13:54:08 +0200

    push pause

--- rank3/philo/routine.c
+++ rank3/philo/routine.c
@@ -6,21 +6,27 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 23:06:41 by jeportie          #+#    #+#             */
-/*   Updated: 2024/09/06 12:03:07 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/09/06 13:49:16 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+ * TODO:
+ * Ajouter fairness system ou autre opti pour passer les tests de la correction
+ */
 
 #include "include/philo.h"
 
 void	ft_check_remaining_locks(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->left_fork->lock_mutex);
-	if (philo->left_fork->is_locked == true && philo->left_fork->philo_id == philo->id)
+	if (philo->left_fork->is_locked == true
+		&& philo->left_fork->philo_id == philo->id)
 		pthread_mutex_unlock(&philo->left_fork->fork_mutex);
 	pthread_mutex_unlock(&philo->left_fork->lock_mutex);
-
 	pthread_mutex_lock(&philo->right_fork->lock_mutex);
-	if (philo->right_fork->is_locked == true && philo->right_fork->philo_id == philo->id)
+	if (philo->right_fork->is_locked == true
+		&& philo->right_fork->philo_id == philo->id)
 		pthread_mutex_unlock(&philo->right_fork->fork_mutex);
 	pthread_mutex_unlock(&philo->right_fork->lock_mutex);
 }
@@ -133,7 +139,6 @@ bool	ft_pick_up_forks(t_philo *philo)
 		philo->left_fork->philo_id = philo->id;
 		pthread_mutex_unlock(&philo->left_fork->lock_mutex);
 	}
-
 	return (true);
 }
 
@@ -141,15 +146,17 @@ void	ft_eat(t_philo *philo)
 {
 	t_mtx	*meal_mtx;
 
-	ft_pick_up_forks(philo);
 	meal_mtx = &philo->mtdata->meal_mutex;
-	philo->meals_eaten++;
-	ft_precise_usleep(philo->rdonly->time_to_eat * 1000);
+	ft_pick_up_forks(philo);
 
 	pthread_mutex_lock(&philo->time_mutex);
 	philo->last_meal_time = ft_get_time_ms();
 	pthread_mutex_unlock(&philo->time_mutex);
 	ft_print_state(philo, EAT);
+
+	philo->meals_eaten++;
+	ft_precise_usleep(philo->rdonly->time_to_eat * 1000);
+
 	if (philo->meals_eaten == philo->rdonly->num_meals)
 		mtx_increment_int(meal_mtx, &philo->mtdata->philos_full);
 	ft_release_forks(philo);
